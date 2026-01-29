4d7e5a9b1c63f57d4a2fece49f6943d06733faaa
isaac-chittilappily-ada <isaac.chittilappily@ada.ac.uk>
2026-01-29 00:57:03 +0000

    added first table in schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,2 +1,14 @@
 .open fittrackpro.db
 .mode column
+
+DROP TABLE IF EXISTS fittrackpro.locations;
+
+
+CREATE TABLE locations(
+    location_id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(13) NOT NULL CHECK (phone_number GLOB '[0-9 ]+')
+    email VARCHAR CHECK (email LIKE '%@%'),
+    opening_hours VARCHAR(11) NOT NULL CHECK (opening_hours GLOB '[0-2][0-9]:[0-5][0-9]-[0-2][0-9]:[0-5][0-9]')
+);
